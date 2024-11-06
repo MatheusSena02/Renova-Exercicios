28da831aa06891f1c56f6093442b366caeae2a73
Matheus Sena <matheus.sena@renova.net.br>
2024-11-06 09:54:22 -0300

    Exercício 3-Continha aprimorado : Não admite entradas negativas ou maiores que 20

--- desenhista.c
+++ desenhista.c
@@ -2,9 +2,22 @@
 
 int main()
 {
-    int altura, espacos, piramide;
+    int altura, espacos, piramide, condicional = 0;
     printf("Altura da pirâmide : ");
     scanf("%d", &altura);
+    if(altura < 1 || altura > 20)
+    {
+        condicional = 1;
+        while(condicional == 1)
+        {
+        printf("Valor inválido. Digite outro valor : ");
+        scanf("%d", &altura);
+            if(altura >= 1 && altura <= 20)
+            {
+                condicional = 0;
+            }
+        }
+    }
     for(int i = 1; i <= altura; i++) 
     {
         espacos = altura - i;
