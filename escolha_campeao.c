d13e6ee4a820d228739c47a365b89ea7cb7b5e40
Matheus Sena <matheus.sena@renova.net.br>
2024-11-11 09:11:23 -0300

    Versão 1 Exercício 8 - 'Escolha do campeão' (resolvido)

--- escolha_campeao.c
+++ escolha_campeao.c
@@ -2,18 +2,46 @@
 
 int main()
 {
-    int campeoes, poder[campeoes], maisforte;
-    printf("Campeões Favoritos de Lucas : ");
-    scanf("%d", &campeoes);
-    for(int i = 0; i <= campeoes; i++)
+    int n, p[100], maisforte = p[0], condicional = 0;
+    printf("N° de campeões favoritos de Lucas : ");
+    scanf("%d", &n);
+    if(n < 1 || n > 100)
     {
-        printf("Nível de Poder do Campeão %d : ", i+1);
-        scanf("%d", &poder[i]);
-        fflush(stdin);
-        if(poder[i] > poder[i - 1]) //possível erro : Busca de valor inexistente
+        condicional = 1;
+        while(condicional == 1)
         {
-            maisforte = poder[i];
+            printf("Valor inválido. Digite um novo valor : ");
+            scanf("%d", &n);
+            if(n > 1 && n < 100)
+            {
+                condicional = 0;
+            }
         }
     }
-    printf("Maior poder : %d", maisforte);
+    for(int i = 0; i < n; i++)
+    {
+        printf("Nível de poder do %d° campeão : ", i+1);
+        scanf("%d", &p[i]);
+        if(p[i] <= 0 || p[i] > 10000)
+        {
+            condicional = 1;
+            while(condicional == 1)
+            {
+                printf("Nível de poder inválido. Digite novamente : ");
+                scanf("%d", &p[i]);
+                if(p[i] > 0 && p[i] <= 10000)
+                {
+                    condicional = 0;
+                }
+            }
+        }
+    }
+    for(int a = n; a >= 0; a--)
+    {
+        if(maisforte <= p[a-1])
+        {
+            maisforte = p[a-1];
+        }
+    }
+    printf("Nível do campeão mais forte : %d", maisforte);
 }
