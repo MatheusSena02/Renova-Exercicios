a51b7fff526fdd7794e8c31c9d79f57ae23d497e
Matheus Sena <matheus.sena@renova.net.br>
2024-11-18 08:22:20 -0300

    perf : Atende as exigências e delimitações  do Exercício 24 - 'Pentatlo'

--- pentatlo.c
+++ pentatlo.c
@@ -6,10 +6,39 @@ int main()
 {
     int x, n[5];
     float somanotas,media;
+    bool condicional = false;
     scanf("%d", &x);
+    if(x < 1 || x > 1000)
+    {
+        condicional = true;
+        while(condicional == true)
+        {
+            printf("Número do candidato inválido. Digite outro valor : ");
+            scanf("%d", &x);
+            if(x >= 1 && x <= 1000)
+            {
+                condicional = false;    
+            }
+        }
+        
+    }
     for(int i = 0; i < 5; i++)
     {
         scanf("%d", &n[i]);
+        if(n[i] < 0 || n[i] > 10)
+        {
+            condicional = true;
+            while(condicional == true)
+            {
+                printf("%d° nota inválida. Digite novamente : ", i+1);
+                printf("\n");
+                scanf("%d", &n[i]);
+                if(n[i] >= 0 && n[i] <= 10)
+                {
+                    condicional = false;  
+                }
+            }
+        }
     }
     for(int a = 0; a < 5; a++)
     {
