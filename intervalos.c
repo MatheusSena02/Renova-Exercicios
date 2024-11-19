b3c692a615a2baf772b537182d667a39491ef5a9
Matheus Sena <matheus.sena@renova.net.br>
2024-11-19 08:54:27 -0300

    feat : Atende os requisitos e Implementa funcionalidades do Exercício 26 - 'Intervalos'

--- intervalos.c
+++ intervalos.c
@@ -4,42 +4,75 @@
 int main()
 {
     int x, y, w, z, a;
+    bool respostaintervalo1 = false, respostaintervalo2 = false, condicional = false;
     scanf("%d %d", &x, &y);
-    scanf("%d %d", &w, &z);
-    scanf("%d", &a);
-    int intervalo1[y-x], intervalo2[z-w];
-    bool respostaintervalo1 = false, respostaintervalo2 = false;
-    intervalo1[y-x] = y;
-    intervalo2[0] = w;
-    for(int i = 0; i <= y-x; i++)
+    if(x >= y)
     {
-        intervalo1[i] = y-x-1+i;
-        if(intervalo1[i] == a)
+        condicional = true;
+        while(condicional == true)
         {
-            respostaintervalo1 = true;    
-            break;
+            printf("Valor inválido, o 1° valor deve ser menor que o 2°. Digite novamente as duas entradas : ");
+            scanf("%d %d", &x, &y);
+            if(x < y)
+            {
+                condicional = false;    
+            }
         }
     }
-    for(int j = 1; j < z-w; j++)
+    scanf("%d %d", &w, &z);
+    if(w >= z)
     {
-        intervalo2[j] = intervalo2[0] + j;    
+        condicional = true; 
+        while(condicional == true)
+        {
+            printf("Valor inválido, o 1° valor deve ser menor que o 2°. Digite novamente as duas entradas: ");
+            scanf("%d %d", &w, &z); 
+            if(w < z)
+            {
+                condicional = false;    
+            }
+            
+        }
+    }
+    scanf("%d", &a);
+    int tamanho1, tamanho2;
+    tamanho1 = y-x;
+    tamanho2 = z-w;
+    int intervalo1[tamanho1], intervalo2[tamanho2];
+    intervalo1[tamanho1-1] = y;
+    for(int i = 0; i <= tamanho1; i++)
+    {
+        intervalo1[i] = y - tamanho1 + i;
+        if(intervalo1[i] == a)
+        {
+            respostaintervalo1 = true;
+        }
+    }
+    for(int j = 0; j < tamanho2; j++)
+    {
+        intervalo2[j] = w + j;
         if(intervalo2[j] == a)
         {
             respostaintervalo2 = true;    
-            break;
         }
     }
     if(respostaintervalo1 == respostaintervalo2)
     {
-        printf("Ambos!");    
-    }else if(respostaintervalo1 == true && respostaintervalo2 == false)
+        if(respostaintervalo1 == true && respostaintervalo2 == true)
+        {
+            printf("Ambos!");    
+        }else if(respostaintervalo1 == false && respostaintervalo2 == false)
+        {
+            printf("Nenhum!");    
+        }
+    }else if(respostaintervalo1 != respostaintervalo2)
     {
-        printf("Primeiro intervalo!");    
-    }else if(respostaintervalo1 == false && respostaintervalo2 == true)
-    {
-        printf("Segundo intervalo!");    
-    }else 
-    {
-        printf("Nenhum!");
+        if(respostaintervalo1 == true && respostaintervalo2 == false)
+        {
+            printf("Primeiro intervalo!");
+        }else if(respostaintervalo1 == false && respostaintervalo2 == true)
+        {
+            printf("Segundo intervalo!");
+        }
     }
 }
